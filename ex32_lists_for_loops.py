f9f4144452b2f7945c434cb2aaf4a899c9caa1e9
Jan Nemec <jnemec79@gmail.com>
2018-02-25 21:02:52 +0100

    The Python Tutorial: 3.1.3 Lists

--- ex32_lists_for_loops.py
+++ ex32_lists_for_loops.py
@@ -6,6 +6,35 @@ the_count = [1, 2, 3, 4, 5]
 fruits = ['apple', 'oranges', 'pears', 'apricots']
 change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
 
+# Lists also support operations like concatenation:
+squares = [1, 4, 9, 16, 25]
+squares + [36, 49, 64, 81, 100]
+
+cubes = [1, 8, 27, 65, 125]
+# Unlike strings, which are immutable, lists are a mutable type, i.e. it is possible to change their content:
+cubes[3] = 64
+
+# You can also add new items at the end of the list, by using the append() method
+cubes.append(216)
+cubes.append(7 ** 3)
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+# Assignment to slices is also possible
+letters[2:5] = ['C', 'D', 'E']
+
+# clear the list by replacing all the elements with an empty list
+letters[:] = []
+
+# The built-in function len() also applies to lists
+len(letters)
+
+# It is possible to nest lists (create lists containing other lists), for example:
+a = ['a', 'b', 'c']
+n = [1, 2, 3]
+x = [a, n]
+x[0]
+x[0][1]
+
 # this first kind of for-loop goes throug a list
 for number in the_count:
     print(f"This is count {number}")
